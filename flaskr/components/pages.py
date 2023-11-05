b854a03f1b15f3bc640f7296d59b77ebd3dbd0a3
drkwitht <derektan3510@gmail.com>
2023-11-04 23:44:26 -0700

    Added templates and extra req handlers.

--- flaskr/components/pages.py
+++ flaskr/components/pages.py
@@ -3,14 +3,12 @@
     Separate part of the Flask code to serve HTML template pages.
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # This router will be set onto the app in __init__.py later.
-page_router = Blueprint('page_router', __name__)
+page_router = Blueprint('page_router', __name__, template_folder='./templates')
 
 # Put request handlers to send back pages.
-@page_router.route('/', methods=['GET', 'POST'])
+@page_router.route('/', methods=['GET'])
 def serve_homepage():
-    return '<h2>Home Page</h2><p>Hello World</p>'
-
-# TODO add more routing functions here to call render_template(<template file name>, <dict of data>) as a server-side rendered reply.
+    return render_template('frontpage.html', page_title='Landing')
--- flaskr/components/routes.py
+++ flaskr/components/routes.py
@@ -12,6 +12,6 @@ from flask import Blueprint
 ajax_router = Blueprint('ajax_router', __name__)
 
 # Put request handler for creating advisories to send back about traffic conditions at some time.
-@ajax_router.route('/appapi/plan/make')
+@ajax_router.route('/appapi/route/make')
 def handle_get_plan():
-    return 'This is a test message.'
+    return {'ok': True, 'msg': 'This is a test message.'} # Placeholder JSON for testing.
