43032544db04101db61e9042a335db96fa2a0e0f
drkwitht <derektan3510@gmail.com>
2023-11-04 23:44:57 -0700

    Added 404 handler.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 # NOTE We may use PyMongo to use the container's MongoDB instance available.
 
 from flaskr.components.pages import page_router
@@ -26,11 +26,15 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # NOTE This requires AJAX on the frontend to send request to the right route URL... See routes.py
-    app.register_blueprint(ajax_router)
-    
+    @app.errorhandler(404)
+    def handle_wrong_route(error):
+        return render_template('oops.html', page_title='Oops Page')
+
     # NOTE This requires the HTML pages to be in the templates folder. Dynamic data can be inserted with Jinja template syntax from the Flask docs. 
     app.register_blueprint(page_router)
+
+    # NOTE This requires AJAX on the frontend to send request to the right route URL... See routes.py
+    app.register_blueprint(ajax_router)
 
     # Export app object to runner.
     return app
